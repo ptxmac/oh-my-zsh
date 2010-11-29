4b12113038cc047a3e51b0182b71d9c3b2220ea6
Peter Kristensen <ptx@cs.au.dk>
2010-11-29 14:53:49 +0100

    added emacs plugin

--- plugins/cuda/cuda.plugin.zsh
+++ plugins/cuda/cuda.plugin.zsh
@@ -1,6 +1,14 @@
 
-CUDADIR=/usr/local/cuda
-if [[ -d $CUDADIR ]]; then
-    export PATH=${PATH}:$CUDADIR/bin
-    export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:/usr/local/cuda/lib
+if [[ $OSTYPE == "linux-gnu" ]]; then
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/cuda/lib"; 
+    export NVSDKCUDA_ROOT="$HOME/NVIDIA_GPU_Computing_SDK/C/"; 
+    export PATH="$HOME/cuda/bin:$PATH";
+else
+    CUDADIR=/usr/local/cuda
+    if [[ -d $CUDADIR ]]; then
+        export PATH=${PATH}:$CUDADIR/bin
+        export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:/usr/local/cuda/lib
+    fi
+
 fi
+
---
+++ plugins/emacs/emacs.plugin.zsh
@@ -0,0 +1,7 @@
+
+if [[ $OSTYPE == "linux-gnu" ]]; then
+    alias cemacs="emacsclient"
+else
+    alias cemacs="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient"
+fi
+
